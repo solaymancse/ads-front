c16958d7cd32b054dfb24e479ba429165199ffbb
solaymancse <mdsolaymancse@gmail.com>
2025-01-13 00:28:07 +0600

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import '@coreui/coreui/dist/css/coreui.min.css';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -27,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` antialiased`}
       >
         <div className="w-full">
           <Header/>
