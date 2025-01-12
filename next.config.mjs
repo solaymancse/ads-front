bc46014a30feb2c8f254fc3dca33c188c219752b
solaymancse <mdsolaymancse@gmail.com>
2025-01-13 01:22:14 +0600

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    fontLoaders: [], // Ensure no font loaders are used
-  },
-  compiler: {
-    styledComponents: true, // Optional: Enable support for styled-components if you're using them
+   
   },
 };
 
