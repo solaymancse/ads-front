2b975e594278540241168ece497aba6b32aee44a
solaymancse <mdsolaymancse@gmail.com>
2025-01-13 01:27:35 +0600

    commit

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,3 +1,9 @@
 export default function NotFound() {
-    return <h1>Whoop! This page doesnt exist! "Put message here with button to redirect to main page"</h1>;
-};  
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+        <p className="mt-4">The page you are looking for does not exist.</p>
+      </div>
+    );
+  }
+  
